9f097d6a05c7b15681bf8fa8f75aaf252eaa311c
JUPJUP <yoonbea12345@gmail.com>
2026-02-19 01:35:35 +0900

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -150,8 +150,8 @@ export default function Home() {
         <div className="relative z-10 max-w-4xl mx-auto">
           <div className="space-y-12 text-center">
             <p className="text-3xl md:text-5xl font-black leading-tight" style={{fontSize: '30px'}}>
-              핫플은 알지만 거기<br />
-              <span className="text-secondary glow-magenta">누가</span> 있는지는 모릅니다.
+              핫플은 알지만 <span className="text-secondary glow-magenta">누가</span><br />
+              있는지는 모릅니다.
             </p>
 
             <p className="text-3xl md:text-5xl font-black leading-tight">
